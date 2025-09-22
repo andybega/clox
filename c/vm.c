58fb8237be26138c342ac73eb7c1c92447324227
Andreas Beger <adbeger@gmail.com>
2025-09-22 14:14:01 +0300

    add arithmetic ops (ch. 15.3)

--- c/vm.c
+++ c/vm.c
@@ -27,10 +27,18 @@ Value pop() {
     return *vm.stackTop;
 }
 
-// This is the heart of the VM
+// This is the heart of the VM: run()
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+    do { \
+      double b = pop(); \
+      double a = pop(); \
+      push(a op b); \
+    } while (false)  // This do {} while (false) trick allows us to use
+                     // multiple statements in a macro, and require
+                     // a semicolon after the macro call.
 
     // Infinite loop to keep reading instructions until we hit a return or error
     for (;;) {
@@ -55,6 +63,11 @@ static InterpretResult run() {
                 push(constant);
                 break;
             }
+            case OP_ADD:      BINARY_OP(+); break;
+            case OP_SUBTRACT: BINARY_OP(-); break;
+            case OP_MULTIPLY: BINARY_OP(*); break;
+            case OP_DIVIDE:   BINARY_OP(/); break;
+            case OP_NEGATE:   push(-pop()); break;
             case OP_RETURN: {
                 printValue(pop());
                 printf("\n");
@@ -65,6 +78,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
