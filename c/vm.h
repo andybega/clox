a9a589933e137a7d347d9e098bb99a75652f06a2
Andreas Beger <adbeger@gmail.com>
2025-10-20 09:42:58 +0300

    add global variables (ch. 21)

--- c/vm.h
+++ c/vm.h
@@ -12,6 +12,7 @@ typedef struct {
     uint8_t* ip;  // instruction pointer showing the _next_ bytecode instruction
     Value stack[STACK_MAX];
     Value* stackTop;
+    Table globals;
     Table strings;
     Obj* objects;
 } VM;
