4bf8ac018f4bd74057e0f5d0fbcbd5e9af9b3dd2
Andreas Beger <adbeger@gmail.com>
2025-11-10 15:45:40 -0500

    start adding gc; mark roots (ch. 26.1-3)

--- c/common.h
+++ c/common.h
@@ -8,6 +8,9 @@
 #define DEBUG_PRINT_CODE
 //#define DEBUG_TRACE_EXECUTION  // Enable this to see trace output
 
+#define DEBUG_STRESS_GC // Enable this to stress test the garbage collector
+#define DEBUG_LOG_GC    // Enable this to see garbage collector logs
+
 #define UINT8_COUNT (UINT8_MAX + 1)
 
 #endif
