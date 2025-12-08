0b6d95b88eab7854be92c653a82f7ecded0cf7f5
Andreas Beger <adbeger@gmail.com>
2025-12-08 11:40:40 +0200

    start NaN boxing and implement number (ch. 30 start)

--- c/value.h
+++ c/value.h
@@ -1,10 +1,44 @@
 #ifndef clox_value_h
 #define clox_value_h
 
+#include <string.h>
+
 #include "common.h"
 
 typedef struct Obj Obj;
 typedef struct ObjString ObjString;
+
+#ifdef NAN_BOXING
+
+// Signature of a quiet NaN
+#define QNAN     ((uint64_t)0x7ffc000000000000)
+
+typedef uint64_t Value;
+
+// Define bit patterns for NaN boxing
+#define IS_NUMBER(value)    (((value) & QNAN) != QNAN)
+
+// Macros for wrapping values
+#define AS_NUMBER(value)    valueToNum(value)
+
+// Macros to unwrap values
+#define NUMBER_VAL(num) numToValue(num)
+
+// Functions that convert between Value and double using NaN boxing
+static inline double valueToNum(Value value) {
+  double num;
+  memcpy(&num, &value, sizeof(Value));
+  return num;
+}
+
+// Function to convert double to Value using NaN boxing
+static inline Value numToValue(double num) {
+    Value value;
+    memcpy(&value, &num, sizeof(double));
+    return value;
+}
+
+#else
 
 typedef enum {
     VAL_BOOL,
@@ -36,6 +70,8 @@ typedef struct {
 #define NIL_VAL           ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
 #define OBJ_VAL(object)   ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+
+#endif
 
 typedef struct {
     int capacity;
