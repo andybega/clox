234d0b8985bc70ca044a51a4d21d64ca4a46d79d
Andreas Beger <adbeger@gmail.com>
2025-12-08 11:05:31 +0200

    faster hash table lookups via bitwise and vs modulo (ch. 30.2)

--- c/table.c
+++ c/table.c
@@ -21,8 +21,8 @@ void freeTable(Table* table) {
 
 static Entry* findEntry(Entry* entries, int capacity,
                         ObjString* key) {
-    // Use modulo to convert hash into array index
-    uint32_t index = key->hash % capacity;
+    // Convert hash into array index
+    uint32_t index = key->hash & (capacity - 1);
     Entry* tombstone = NULL;
 
     for (;;) {
@@ -40,7 +40,7 @@ static Entry* findEntry(Entry* entries, int capacity,
             return entry;
         }
 
-        index = (index + 1) % capacity;
+        index = (index + 1) & (capacity - 1);
     }
 }
 
@@ -125,7 +125,7 @@ ObjString* tableFindString(Table* table, const char* chars,
                            int length, uint32_t hash) {
     if (table->count == 0) return NULL;
 
-    uint32_t index = hash % table->capacity;
+    uint32_t index = hash & (table->capacity - 1);
     for (;;) {
         Entry* entry = &table->entries[index];
         if (entry->key == NULL) {
@@ -138,7 +138,7 @@ ObjString* tableFindString(Table* table, const char* chars,
             return entry->key;
         }   
 
-        index = (index + 1) % table->capacity;
+        index = (index + 1) & (table->capacity - 1);
     }
 }
 
